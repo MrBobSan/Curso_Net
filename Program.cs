aa9810ebb065bc068a1499288fc9d1492270e16a
Daniel Serrano <dserranoc@indra.es>
2026-02-05 12:46:24 +0100

    Fundamentals y llamada a su ejecucion en .NET

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 /* Console.WriteLine("Hello, World!"); */
+using System;
+using static System.Console;
+using CsBases.Fundamentals;
+
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
         // Declaramos Explicita
         int cantidad = 15;
@@ -15,5 +19,10 @@ class Program
         var mensaje1 = "La cantidad con var es: ";
         var precio1 = 29.99m;
         System.Console.WriteLine($"Cantidad1: {cantidad1}, mensaje1: {mensaje1}, precio1: {precio1}");
+
+        var laptop = new Product("Laptop", 999.99m);
+        System.Console.WriteLine(laptop.GetDescription());
+        var soporte = new ServiceProduct("Soporte Técnico", 300.99m, 30);
+        System.Console.WriteLine(soporte.GetDescription());
     }
 }
